63047f3210005b8180b6920d023bdae8388a58e2
Hugo U. R. Strand <hugo.strand@gmail.com>
2023-05-12 18:43:12 +0200

    [dlr] add tensor_valued py converters

--- python/triqs/gf/gf_fnt_desc.py
+++ python/triqs/gf/gf_fnt_desc.py
@@ -186,17 +186,21 @@ for Target in  ["scalar_valued", "matrix_valued", "tensor_valued<3>", "tensor_va
 
 m.add_function("gf<dlr_coeffs> dlr_coeffs_from_dlr_imtime(gf_const_view<dlr_imtime> g_tau)")
 m.add_function("gf<dlr_coeffs, scalar_valued> dlr_coeffs_from_dlr_imtime(gf_const_view<dlr_imtime, scalar_valued> g_tau)")
+m.add_function("gf<dlr_coeffs, tensor_valued<4>> dlr_coeffs_from_dlr_imtime(gf_const_view<dlr_imtime, tensor_valued<4>> g_tau)")
 
 m.add_function("gf<dlr_imtime> dlr_imtime_from_dlr_coeffs(gf_const_view<dlr_coeffs> g_dlr)")
 m.add_function("gf<dlr_imtime, scalar_valued> dlr_imtime_from_dlr_coeffs(gf_const_view<dlr_coeffs, scalar_valued> g_dlr)")
+m.add_function("gf<dlr_imtime, tensor_valued<4>> dlr_imtime_from_dlr_coeffs(gf_const_view<dlr_coeffs, tensor_valued<4>> g_dlr)")
 
 # dlr_imfreq <-> dlr_coeffs
 
 m.add_function("gf<dlr_coeffs> dlr_coeffs_from_dlr_imfreq(gf_const_view<dlr_imfreq> g_iw)")
 m.add_function("gf<dlr_coeffs, scalar_valued> dlr_coeffs_from_dlr_imfreq(gf_const_view<dlr_imfreq, scalar_valued> g_iw)")
+m.add_function("gf<dlr_coeffs, tensor_valued<4>> dlr_coeffs_from_dlr_imfreq(gf_const_view<dlr_imfreq, tensor_valued<4>> g_iw)")
 
 m.add_function("gf<dlr_imfreq> dlr_imfreq_from_dlr_coeffs(gf_const_view<dlr_coeffs> g_dlr)")
 m.add_function("gf<dlr_imfreq, scalar_valued> dlr_imfreq_from_dlr_coeffs(gf_const_view<dlr_coeffs, scalar_valued> g_dlr)")
+m.add_function("gf<dlr_imfreq, tensor_valued<4>> dlr_imfreq_from_dlr_coeffs(gf_const_view<dlr_coeffs, tensor_valued<4>> g_dlr)")
 
 
 ########################
